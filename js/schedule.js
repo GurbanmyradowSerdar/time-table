b8c233b7f1b6d941aae2626d70d513ca82a2d53b
Serdar <115923465+GurbanmyradowSerdar@users.noreply.github.com>
2024-03-20 16:26:56 +0500

    feat(schedule.js): changed schedule, global variables

--- js/schedule.js
+++ js/schedule.js
@@ -1,36 +1,3 @@
-const sanawjy = [
-  {
-    first_pair: "Filosofiýa (umumy) / B11",
-    second_pair: "MG we banklary (amaly) / A04",
-    third_pair: "Elektrotehnika we elektronikanyň esaslary (amaly) / W305",
-  },
-  {
-    first_pair: "Elektrotehnika we elektronikanyň esaslary (umumy) / B11",
-    second_pair:
-      "Operasion ulgamlar (tejribe) / A09\nOperasion ulgamlar (tejribe) / B10",
-    third_pair: "Elektrotehnika we elektronikanyň esaslary (umumy) / B11",
-  },
-  {
-    first_pair: "Awtomatiki dolandyryşyň nazaryýeti SD (amaly) / B04",
-    second_pair:
-      "MG we banklary (tejribe) / A11\nElektrotehnika we elektronikanyň esaslary (tejribe) / W305",
-    third_pair: "Operasion ulgamlar (amaly) / A11",
-  },
-  {
-    first_pair: "MG we banklary (umumy) / B11",
-    second_pair: "Bedenterbiýe / FOK",
-    third_pair: "Elektrotehnika we elektronikanyň esaslary (amaly) / W305",
-  },
-  {
-    first_pair: "Önümçilik / praktika",
-  },
-  {
-    first_pair: "Operasion ulgamlar (umumy) / B11",
-    second_pair:
-      "Elektrotehnika we elektronikanyň esaslary (tejribe) / W305\nMG we banklary (tejribe) / A09",
-    third_pair: "Iňlis dili (amaly) / A08",
-  },
-];
 const maydalawjy = [
   {
     first_pair: "Filosofiýa (umumy) / B11",
@@ -54,6 +21,40 @@ const maydalawjy = [
     first_pair: "MG we banklary (umumy) / B11",
     second_pair: "Bedenterbiýe / FOK",
     third_pair: "Operasion ulgamlar (umumy) / B11",
+  },
+  {
+    first_pair: "Önümçilik / praktika",
+  },
+  {
+    first_pair: "Operasion ulgamlar (umumy) / B11",
+    second_pair:
+      "Elektrotehnika we elektronikanyň esaslary (tejribe) / W305\nMG we banklary (tejribe) / A09",
+    third_pair: "Iňlis dili (amaly) / A08",
+  },
+];
+
+const sanawjy = [
+  {
+    first_pair: "Filosofiýa (umumy) / B11",
+    second_pair: "MG we banklary (amaly) / A04",
+    third_pair: "Elektrotehnika we elektronikanyň esaslary (amaly) / W305",
+  },
+  {
+    first_pair: "Elektrotehnika we elektronikanyň esaslary (umumy) / B11",
+    second_pair:
+      "Operasion ulgamlar (tejribe) / A09\nOperasion ulgamlar (tejribe) / B10",
+    third_pair: "Elektrotehnika we elektronikanyň esaslary (umumy) / B11",
+  },
+  {
+    first_pair: "Awtomatiki dolandyryşyň nazaryýeti SD (amaly) / B04",
+    second_pair:
+      "MG we banklary (tejribe) / A11\nElektrotehnika we elektronikanyň esaslary (tejribe) / W305",
+    third_pair: "Operasion ulgamlar (amaly) / A11",
+  },
+  {
+    first_pair: "MG we banklary (umumy) / B11",
+    second_pair: "Bedenterbiýe / FOK",
+    third_pair: "Elektrotehnika we elektronikanyň esaslary (amaly) / W305",
   },
   {
     first_pair: "Önümçilik / praktika",
